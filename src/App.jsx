20db19f2f10b3a1555b8828d5c1763eed3f20611
kasvrol <dovalekarolina@gmail.com>
2022-04-28 18:30:39 -0300

    [FEAT 003.1] rotas

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/home'
+import WhoWeAre from './pages/whoWeAre'
+import WhatWeDo from './pages/whatWeDo'
+import OurSeamstresses from './pages/OurSeamstresses'
+
+function App() {
+  return (
+    <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/whoWeAre' element={<WhoWeAre/>}/>
+        <Route path='/whatWeDo' element={<WhatWeDo/>}/>
+        <Route path='/ourSeamstresses' element={<OurSeamstresses/>}/>
+    </Routes>
+  );
+}
+
+export default App;
