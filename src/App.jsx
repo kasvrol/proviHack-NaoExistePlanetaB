f2bef68c45ee6cef0df6a68b1b9f504b4e8d88a8
kasvrol <dovalekarolina@gmail.com>
2022-04-30 10:46:36 -0300

    [REFACTOR][TISSUES] mudança no nome do arquivo e rota

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import {Routes, Route} from 'react-router-dom'
-import Home from './pages/home'
-import WhoWeAre from './pages/whoWeAre'
-import HowDo from './pages/howDo'
-import OurSeamstresses from './pages/OurSeamstresses'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import WhoWeAre from "./pages/whoWeAre";
+import Tissues from "./pages/tissues";
+import OurSeamstresses from "./pages/OurSeamstresses";
 
 function App() {
-  return (
-    <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/whoWeAre' element={<WhoWeAre/>}/>
-        <Route path='/howDo' element={<HowDo/>}/>
-        <Route path='/ourSeamstresses' element={<OurSeamstresses/>}/>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/whoWeAre" element={<WhoWeAre />} />
+            <Route path="/tissues" element={<Tissues />} />
+            <Route path="/ourSeamstresses" element={<OurSeamstresses />} />
+        </Routes>
+    );
 }
 
 export default App;
