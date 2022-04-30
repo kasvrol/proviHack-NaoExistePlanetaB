6679ce069859abe0ca23a99c706b8e12492fab31
kasvrol <dovalekarolina@gmail.com>
2022-04-30 19:12:19 -0300

    [FEAT][HOME] estilo do primeiro terço da página home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,17 +5,23 @@ import { WhoWeAre, WhatWeDo } from "../components/whoWeAreText";
 import image05 from "../images/image05.svg";
 import image06 from "../images/image06.svg";
 
+import "../style/home.scss";
+
 function Home() {
     return (
         <main>
             <Logo />
-            <WhoWeAre />
-            <img src={image06} alt="ilustração de um monitor" />
-            <WhatWeDo />
-            <img
-                src={image05}
-                alt="ilustração de um fotográfo tirando fot de roupas"
-            />
+            <section className="informationsContainer">
+                <WhoWeAre />
+                <img src={image06} alt="ilustração de um monitor" />
+            </section>
+            <section className="informationsContainer">
+                <img
+                    src={image05}
+                    alt="ilustração de um fotográfo tirando fot de roupas"
+                />
+                <WhatWeDo />
+            </section>
         </main>
     );
 }
