6794b66e90f0a4b2f449a8f6ecb8dd7ddcc47f46
kasvrol <dovalekarolina@gmail.com>
2022-05-01 18:14:00 -0300

    [FEAT][WHO WE ARE] responsividade nos cards

--- src/pages/whoWeAre.jsx
+++ src/pages/whoWeAre.jsx
@@ -7,6 +7,11 @@ import Button from "../components/Button";
 import Logo from "../components/Logo";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import ImageCard from "../components/Card/image-card";
+
+import instruction01 from "../images/image01.svg";
+import instruction02 from "../images/image02.svg";
+import instruction03 from "../images/image03.svg";
 import "../style/custom.scss";
 
 function whoWeAre() {
@@ -17,7 +22,32 @@ function whoWeAre() {
                 <Logo />
                 <WhoWeAre classNameTitle="WhoWeAreTitle" />
                 <Informations />
-                <Instructions />
+                <ImageCard
+                    title={"Como nosso projeto funciona?"}
+                    classTitle={"titlePink"}
+                    imageOne={instruction01}
+                    imageTwo={instruction02}
+                    imageThree={instruction03}
+                    textImageOne={
+                        "1 -Empresas doam residúdos de tecidos (recortes)."
+                    }
+                    textImageTwo={
+                        "2 -Costureiras se cadastram para receber doações."
+                    }
+                    textImageThree={
+                        "3 -Costureiras retiram no polo mais próximo de suas casas."
+                    }
+                    textOne={
+                        "1 -Empresas doam residúdos de tecidos (recortes)."
+                    }
+                    textTwo={
+                        "2 -Costureiras se cadastram para receber doações."
+                    }
+                    textThree={
+                        "3 -Costureiras retiram no polo mais próximo de suas casas."
+                    }
+                    cardParagraph={"card-paragraph"}
+                />
                 <TissuesCard />
                 <AboutMore classNameTitle="WhoWeAreTitle" />
                 <Button
