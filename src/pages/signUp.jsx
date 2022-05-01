e896ad82c378fa5af499f71c46e3da73efd38541
kasvrol <dovalekarolina@gmail.com>
2022-05-01 07:06:37 -0300

    [FEAT][SIGN UP] estilo da página

--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -1,39 +1,94 @@
 import React from "react";
 import Logo from "../components/Logo";
 
-import image05 from "../images/image05.svg";
+import image07 from "../images/image07.svg";
 
-import "../style/home.scss";
+import "../style/signUp.scss";
 
 function Home() {
     return (
         <main className="signUpContainer">
             <Logo />
             <img
-                src={image05}
+                src={image07}
                 alt="ilustração de um fotográfo tirando fot de roupas"
+                section
+                className="registrationImage"
             />
-            <form>
-                <section>
+            <p className="registrationParagraph">Cadastre-se</p>
+            <form className="signUpForm">
+                <section className="registrationTypeContainer">
                     <p>Selecione abaixo qual grupo você pertence</p>
-                    <label>Costureira</label>
-                    <input type="radio" name="costureira" />
-                    <label>Empresa doadora de tecidos</label>
-                    <input type="radio" name="empresa-doadora" />
+                    <section className="registrationType">
+                        <label for="costureira">Costureira</label>
+                        <input
+                            type="radio"
+                            name="costureira"
+                            id="costureira"
+                            value="costureira"
+                            name="registration_type"
+                        />
+                    </section>
+                    <section className="registrationType">
+                        <label for="empresaDoadora">
+                            Empresa doadora de tecidos
+                        </label>
+                        <input
+                            type="radio"
+                            name="empresa-doadora"
+                            id="empresaDoadora"
+                            value="empresaDoadora"
+                            name="registration_type"
+                        />
+                    </section>
                 </section>
 
-                <input type="text" placeholder="Nome Completo" />
-                <input type="text" placeholder="CPF ou CNPJ" />
-                <input type="text" placeholder="CEP" />
-                <input type="text" placeholder="Endereço" />
-                <input />
-                <input type="text" placeholder="Complemento" />
-                <input type="text" placeholder="Senha" />
-                <input type="text" placeholder="Confirme sua Senha" />
+                <input
+                    type="text"
+                    placeholder="Nome Completo"
+                    className="informationsSection"
+                />
+                <input
+                    type="text"
+                    placeholder="CPF ou CNPJ"
+                    className="informationsSection"
+                />
+                <input
+                    type="text"
+                    placeholder="CEP"
+                    className="informationsSection"
+                />
+                <input
+                    type="text"
+                    placeholder="Endereço"
+                    className="informationsSection"
+                />
+                <section className="informationsAdressSection">
+                    <input
+                        type="number"
+                        placeholder="N:"
+                        className="informationsAdressSectionInput"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Complemento"
+                        className="informationsAdressSectionInput"
+                    />
+                </section>
+                <input
+                    type="text"
+                    placeholder="Senha"
+                    className="informationsSection"
+                />
+                <input
+                    type="text"
+                    placeholder="Confirme sua Senha"
+                    className="informationsSection"
+                />
 
-                <input type="submit" placeholder="Concluir!" />
+                <button className="submitButton">Concluir!</button>
             </form>
-            <button>Cancelar</button>
+            <button className="cancelButton">Cancelar</button>
         </main>
     );
 }
